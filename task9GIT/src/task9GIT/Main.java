07a4112a43aad7e37990e100e6011e29d3190438
molly-smith-x <obmolly92@gmail.com>
2025-06-25 20:37:38 +1200

    Added user input and then used response in an output.

--- task9GIT/src/task9GIT/Main.java
+++ task9GIT/src/task9GIT/Main.java
@@ -1,8 +1,21 @@
 package task9GIT;
 
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
+    	Scanner scanner = new Scanner(System.in);
+    	
     	System.out.println("Hello from Task 9!");
     	System.out.println("How are you today?");
+    	String feeling = scanner.nextLine();
+    	
+    	System.out.println("You are " + feeling + "! This is Task 9 for BIT504.");
+
+    	scanner.close();
     }
 }
+
+
+
