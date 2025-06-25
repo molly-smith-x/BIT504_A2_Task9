3d7d495d9518127901f91ff5c9e34792558d6a92
molly-smith-x <obmolly92@gmail.com>
2025-06-25 20:25:22 +1200

    Making further changes to the output.

--- task9GIT/src/task9GIT/Main.java
+++ task9GIT/src/task9GIT/Main.java
@@ -3,5 +3,6 @@ package task9GIT;
 public class Main {
     public static void main(String[] args) {
     	System.out.println("Hello from Task 9!");
+    	System.out.println("How are you today?");
     }
 }
